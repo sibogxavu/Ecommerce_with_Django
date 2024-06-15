37a3a31c6fda93662f6b482edce4691225dcbd1d
sibogxavu <s.gxavu@gmail.com>
2024-06-15 12:18:30 +0200

    Update Customers

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'Ecommerce'
 
 urlpatterns = [
-    path('product_list/', views.product_list, name='product_list'),
-
+    path('', views.product_list, name='product_list'),
+    path('<int:pk>/', views.product_detail, name='product_detail'),
+    path('customers/', views.customer_list, name='customer_list'),
+    path('customers/<int:pk>', views.customer_detail, name='customer_detail'),
 ]
